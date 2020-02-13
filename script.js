ae423c975b2154945891b9924ad5609a19af5f2b
unknown <mcculeth000@warren.k12.in.us>
2020-02-13 10:58:39 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let score = 0;
+let questionText = document.getElementById("question");
+let choice1 = document.getElementById("btn0");
+let choice2 = document.getElementById("btn1");
+let choice3 = document.getElementById("btn2");
+let choice4 = document.getElementById("btn3");
+let results = ' ';
+
+
+
+let Q1 = function(){
+    questionText.innerText = "Hello World?";
+    choice1.innerText = "Hello World";
+    choice2.innerText = "Hi World";
+    choice3.innerText = "Good Morning World";
+    choice4.innerText = "Ohaiyo!!!";
+};
+
+let Q2 = function(){
+    questionText.innerText = "Hello World? 2";
+    choice1.innerText = "Hello World 2";
+    choice2.innerText = "Hi World 2";
+    choice3.innerText = "Good Morning World 2";
+    choice4.innerText = "Ohaiyo!!! 2";
+};
+
+let Q3 = function(){
+    questionText.innerText = "Hello World? 3";
+    choice1.innerText = "Hello World 3";
+    choice2.innerText = "Hi World 3";
+    choice3.innerText = "Good Morning World 3";
+    choice4.innerText = "Ohaiyo!!! 3";
+};
+
+let Q4 = function(){
+    questionText.innerText = "Hello World? 4";
+    choice1.innerText = "Hello World 4";
+    choice2.innerText = "Hi World 4";
+    choice3.innerText = "Good Morning World 4";
+    choice4.innerText = "Ohaiyo!!! 4";
+};
+
+
