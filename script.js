1ba94d71d4fe72329dba322cb400ee7d4b8b73f3
mcculeth000 <mcculeth000@warren.k12.in.us>
2020-02-26 10:21:29 -0500

    Finished

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-let score = 0;
-let questionText = document.getElementById("question");
-let choice1 = document.getElementById("btn0");
-let choice2 = document.getElementById("btn1");
-let choice3 = document.getElementById("btn2");
-let choice4 = document.getElementById("btn3");
-let results = ' ';
+let count = 0;
+let numRight = 0;
+let correctOne;
+let numQuestions = 4;
 
+let btn1 = document.getElementById("btn0");
+let btn2 = document.getElementById("btn1");
+let btn3 = document.getElementById("btn2");
+let btn4 = document.getElementById("btn3");
+let Question = document.getElementById("question");
 
-
-let Q1 = function(){
-    questionText.innerText = "Hello World?";
-    choice1.innerText = "Hello World";
-    choice2.innerText = "Hi World";
-    choice3.innerText = "Good Morning World";
-    choice4.innerText = "Ohaiyo!!!";
+let createQuestion = function(questionText, choice1, choice2, choice3, choice4, correctChoice){
+    Question.innerText = questionText;
+    btn1.innerText = choice1;
+    btn2.innerText = choice2;
+    btn3.innerText = choice3;
+    btn4.innerText = choice4;
+    correctOne = correctChoice;
 };
 
-let Q2 = function(){
-    questionText.innerText = "Hello World? 2";
-    choice1.innerText = "Hello World 2";
-    choice2.innerText = "Hi World 2";
-    choice3.innerText = "Good Morning World 2";
-    choice4.innerText = "Ohaiyo!!! 2";
+let DoTheStuff = function(){
+
+    btn1.onclick = function(){checkCorrect(this)};
+    btn2.onclick = function(){checkCorrect(this)};
+    btn3.onclick = function(){checkCorrect(this)};
+    btn4.onclick = function(){checkCorrect(this)};
+
+    if(count === 0){
+        createQuestion("What is my name?" , "Jack", "Nathan", "Ethan", "John", "Ethan" );
+        correctOne = btn3;
+    }else if(count === 1){
+        createQuestion("What is my favorite animal?", "Raccoon", "Sloth", "Koala", "Red Panda", "Raccoon");
+        correctOne = btn1;
+    }else if(count === 2){
+        createQuestion("What is my favorite color?", "Blue", "Yellow", "Red", "Purple", "Purple");
+        correctOne = btn4;
+    }else if(count === 3){
+        createQuestion("What is my favorite subject?", "Math", "Language Arts", "Computer Science (You should pick this)", "Astronomy", "Computer Science (You should pick this)");
+        correctOne = btn3;
+    }else{
+        let question = document.getElementById("question");
+        question.innerText = "You got " + numRight + " correct out of " + numQuestions;
+        document.getElementById("buttons").innerHTML = " ";
+    }
+
+    document.getElementById("progress").innerText = "Question " + count + " of " + numQuestions;
+
 };
 
-let Q3 = function(){
-    questionText.innerText = "Hello World? 3";
-    choice1.innerText = "Hello World 3";
-    choice2.innerText = "Hi World 3";
-    choice3.innerText = "Good Morning World 3";
-    choice4.innerText = "Ohaiyo!!! 3";
+let checkCorrect = function(button){
+    if(button === correctOne){
+        numRight++;
+    }
+    count++;
+    DoTheStuff();
 };
-
-let Q4 = function(){
-    questionText.innerText = "Hello World? 4";
-    choice1.innerText = "Hello World 4";
-    choice2.innerText = "Hi World 4";
-    choice3.innerText = "Good Morning World 4";
-    choice4.innerText = "Ohaiyo!!! 4";
-};
-
-
+//function(questionText, choice1, choice2, choice3, choice4, correctChoice){ *your code goes here* }
